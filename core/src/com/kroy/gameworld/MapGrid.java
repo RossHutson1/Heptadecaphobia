0eba3425ebfb8e9a748021d4d789be21ba6c4426
4dambarr <ab2474@cse069pc-04.its.york.ac.uk>
2020-01-17 17:27:22 +0000

    map grid

--- core/src/com/kroy/gameworld/MapGrid.java
+++ core/src/com/kroy/gameworld/MapGrid.java
@@ -1,10 +1,29 @@
 package com.kroy.gameworld;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+
 public class MapGrid {
 	private int[][] grid;
 	
 	public MapGrid() {
-		int[][] grid = new int[36][43];
+		int[][] grid = new int[45][45];
+		
+		FileHandle handle = Gdx.files.local("mapGrid.csv");
+		String text = handle.readString();
+		String cellsArray[] = text.split(",|\\r?\\n");
+		for (int i = 0; i < cellsArray.length; i++) {
+			grid[i/43][i%43] = Integer.parseInt(cellsArray[i]);
+		}
+		
+		for (int x = 0; x < 43; x++) {
+			for (int y = 0; y < 36; y++) {
+				System.out.print(grid[y][x]);
+			}
+			System.out.println();
+		}
+		
+		
 		
 	}
 }
